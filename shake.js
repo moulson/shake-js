22aca222da095fa170c6db095ff0f7c9d9c34e08
moulson <dan@danmoulson.com>
2021-01-05 17:05:48 +0000

    initial

---
+++ shake.js
@@ -0,0 +1,23 @@
+$.fn.shake = function(interval = 150){
+    this.addClass('shaking');
+    this.css('transition', 'all ' + (interval / 100).toString() + 's');
+    setTimeout(() => {
+        this.css('transform', 'translateX(-50%)');
+    }, interval * 0);
+    setTimeout(() => {
+        this.css('transform', 'translateX(50%)');
+    }, interval * 1);
+    setTimeout(() => {
+        this.css('transform', 'translateX(-25%)');
+    }, interval * 2);
+    setTimeout(() => {
+        this.css('transform', 'translateX(25%)');
+    }, interval * 3);
+    setTimeout(() => {
+        this.css('transform', 'translateX(-7%)');
+    }, interval * 4);
+    setTimeout(() => {
+        this.css('transform', 'translateX(0%)');
+    }, interval * 5);
+    this.removeClass('shaking');
+};
